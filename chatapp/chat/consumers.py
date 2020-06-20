47ddc2e4f575f59df430443f0f33bdb4d3533d7e
sanix sandel <sanicksikani@gmail.com>
2020-06-20 10:59:35 +0300

    working !

--- chatapp/chat/consumers.py
+++ chatapp/chat/consumers.py
@@ -4,44 +4,45 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_name=self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name='chat_%s' % self.room_name
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
 
-        #join room group
+        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
+        self.accept()
 
     def disconnect(self, close_code):
-        #leave room group
+        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    #receive message from websocket
+    # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json=json.loads(text_data)
-        message=text_data_json['message']
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-        #send message to room group
+        # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type':'chat_message',
-                'message':message
+                'type': 'chat_message',
+                'message': message
             }
         )
 
-    #Receive message from room group
+    # Receive message from room group
     def chat_message(self, event):
-        message=event['message']
+        message = event['message']
 
-        #send message to WebSocket
+        # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message':message
+            'message': message
         }))
 
 #When a user posts a message, a JavaScript function will
@@ -52,3 +53,49 @@ class ChatConsumer(WebsocketConsumer):
 #will then receive the message from the group and forward it
 #over WebSocket back to JavaScript,
 # where it will be appended to the chat log.
+
+
+#****self.scope['url_route']['kwargs']['room_name']
+#Obtains the 'room_name' parameter from the URL route in chat/routing.py that opened
+#the WebSocket connection to the consumer.
+#Every consumer has a scope that contains information about its connection,
+#including in particular any positional or keyword arguments
+#from the URL route and the currently authenticated user if any.
+
+
+#****self.room_group_name = 'chat_%s' % self.room_name
+#Constructs a Channels group name directly from the user-specified room name,
+#without any quoting or escaping.
+#Group names may only contain letters, digits, hyphens, and periods.
+#Therefore this example code will fail on room names that have other characters.
+
+
+#****async_to_sync(self.channel_layer.group_add)(...)
+#Joins a group. The async_to_sync(…) wrapper is required
+#because ChatConsumer is a synchronous WebsocketConsumer
+#but it is calling an asynchronous channel layer method.
+#(All channel layer methods are asynchronous.)
+#Group names are restricted to ASCII alphanumerics, hyphens, and periods only.
+#Since this code constructs a group name directly
+#from the room name, it will fail if the room name
+#contains any characters that aren’t valid in a group name.
+
+
+#****self.accept()
+#Accepts the WebSocket connection.
+#If you do not call accept() within the connect() method then
+ #the connection will be rejected and closed.
+ #You might want to reject a connection for example
+ #because the requesting user is not authorized to perform the requested action.
+#It is recommended that accept() be called as
+#the last action in connect() if you choose to accept the connection.
+
+
+#async_to_sync(self.channel_layer.group_discard)(...)
+#Leaves a group.
+
+
+#async_to_sync(self.channel_layer.group_send)
+#Sends an event to a group.
+#An event has a special 'type' key corresponding to the name of
+#the method that should be invoked on consumers that receive the event.
