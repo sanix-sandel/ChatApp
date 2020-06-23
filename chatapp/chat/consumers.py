a063f67aa5f4807d5c8774d41455a67f041be805
sanix sandel <sanicksikani@gmail.com>
2020-06-24 02:48:49 +0300

    javascript issue

--- chatapp/chat/consumers.py
+++ chatapp/chat/consumers.py
@@ -7,43 +7,69 @@ from django.contrib.auth.models import User
 
 class ChatConsumer(WebsocketConsumer):
 
-    def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        self.accept()
-
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-
-
     def fetch_messages(self, data):
-        messages=Message.objects.all()
-        content={
-            'messages':self.messages_to_json(messages)
+        messages = Message.last_10_messages()#Fetch the data from 
+        #the database
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
         }
-        self.send_message(content)
+        self.send_message(content)#Send the jsonified data to websocket 
 
-    def messages_to_json(self, messages):#serialize all messages
-        result=[]
+    def new_message(self, data):
+        author = data['from']
+        author_user = User.objects.filter(username=author)[0]
+        message = Message.objects.create(
+            author=author_user, 
+            content=data['message'])
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
         for message in messages:
             result.append(self.message_to_json(message))
         return result
 
-    def send_chat_message(self, message):#Send retrieved messages stored in
-        # the database to room group
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )   
+
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
+
+    def send_chat_message(self, message): #Send retrieved messages 
+        #stored in
+        # the database to room group   
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -52,53 +78,14 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps(message))
-
-
-
-
-    def new_message(self, data):
-        author=data['from']
-        author_user=User.objects.filter(username=author)[0]
-        message=Message.objects.create(
-            author=author_user,
-            content=data['message']
-        )
-        content={
-            'command':'new_message',
-            'message':self.message_to_json(message)
-        }
-        return self.send_chat_message(content)
-        #print('new message')
-
-    def message_to_json(self, message):
-        return {
-            'author':message.author.username,
-            'content':message.content,
-            'timestamp':str(message.timestamp)
-        }
-
-    commands={
-        'fetch_messages':fetch_messages,
-        'new_message':new_message
-    }
-
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        data = json.loads(text_data)
-        print(data)
-        self.commands[data['command']](data)
-
-
     def send_message(self, message):
         self.send(text_data=json.dumps(message))
 
 
+    ## Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))                 
 
 
 #This new code is for ChatConsumer is very similar to the original code,
